20a1ffedbe332fdde7828947bb3dc59b9f28b47d
anassalamah <anas.salamah@gmail.com>
2024-01-02 01:05:20 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '' : undefined,
+};
