87c5fa2bba63e1999351ed14ecf0f10b0264972f
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2024-01-02 21:18:41 +0500

    fix the issue, static files not load

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 // next.config.js
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://sahmk.sa/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://sahmk.sa/' : 'http://localhost:3000/',
 };
