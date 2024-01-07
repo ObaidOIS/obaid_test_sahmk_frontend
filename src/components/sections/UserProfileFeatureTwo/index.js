69da5b464c9715ba33a9a3f02ea247ed2ec1ce55
Huzaifa Zahoor <huzaifazahoor654@gmail.com>
2024-01-07 23:55:59 +0500

    fix the popup model

--- src/components/sections/UserProfileFeatureTwo/index.js
+++ src/components/sections/UserProfileFeatureTwo/index.js
@@ -7,7 +7,8 @@ import ModalUI from "@/components/widgets/ModalUI";
 import PopupModal from "@/components/widgets/PopupModal";
 import FeatureTwoGoalModal from "../FeatureTwoGoalModal";
 import apiCall from "@/components/common/api";
-
+import MessageAlert from "@/components/widgets/MessageAlert";
+import { XCircleIcon, CheckCircleIcon } from "@heroicons/react/20/solid";
 const UserProfileFeatureTwo = ({
   isPricesChecked,
   handlePricesSwitch,
@@ -18,6 +19,18 @@ const UserProfileFeatureTwo = ({
   const [selectedItems, setSelectedItems] = useState([]);
   const [activeButton, setActiveButton] = useState("");
   const [options, setOptions] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [formData, setFormData] = useState({
+    id: "",
+    symbol: "",
+    name: "",
+    stock_price: "",
+    target_price: "",
+  });
+  const [errorAlert, setErrorAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successAlert, setSuccessAlert] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
 
   const buttonTabs = [
     { button: "-10%" },
@@ -30,6 +43,19 @@ const UserProfileFeatureTwo = ({
   // Fetch companies from API and set to both dataList and originalData
   useEffect(() => {
     const fetchCompanies = async () => {
+      const userStocksResponse = await apiCall("/api/stocks/");
+      if (userStocksResponse.status === 200 && userStocksResponse.result) {
+        const userStocks = userStocksResponse.result.stocks.map((stock) => ({
+          id: stock.id,
+          symbol: stock.symbol,
+          target_price: stock.target_price,
+          name: stock.stock_name,
+          stock_price: stock.stock_price,
+        }));
+        setSelectedItems(userStocks);
+        setTableData(userStocks);
+      }
+
       const response = await apiCall("/api/stocks/get-stocks-list?price=yes");
       if (response.result) {
         const formattedData = response.result.map(
@@ -64,13 +90,13 @@ const UserProfileFeatureTwo = ({
         const newGoalPrice = currentPrice * (1 + percentage);
         setFormData((prevData) => ({
           ...prevData,
-          goal: newGoalPrice.toFixed(2), // Update the goal in the state
+          target_price: newGoalPrice.toFixed(2), // Update the goal in the state
         }));
       } else if (value === "سعر مخصص") {
         // Clear or set to custom mode
         setFormData((prevData) => ({
           ...prevData,
-          goal: "", // Clear the goal or set to a default custom value
+          target_price: "", // Clear the goal or set to a default custom value
         }));
       }
     }
@@ -79,14 +105,6 @@ const UserProfileFeatureTwo = ({
   const handleFeedClick = () => {
     setIsSecondFeatureModalOpen(true);
   };
-
-  const [formData, setFormData] = useState({
-    company: "",
-    price: "",
-    goal: "",
-  });
-
-  const [tableData, setTableData] = useState([]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -98,7 +116,7 @@ const UserProfileFeatureTwo = ({
       const selectedOption = e.target.options[e.target.selectedIndex];
       const price = selectedOption.getAttribute("data-price");
       if (price) {
-        setFormData((prevData) => ({ ...prevData, price: price }));
+        setFormData((prevData) => ({ ...prevData, current_price: price }));
       }
     }
   };
@@ -107,9 +125,11 @@ const UserProfileFeatureTwo = ({
     // e.preventDefault();
     setTableData((prevPeople) => [...prevPeople, formData]);
     setFormData({
-      company: "",
-      price: "",
-      goal: "",
+      id: "",
+      symbol: "",
+      name: "",
+      stock_price: "",
+      target_price: "",
     });
   };
 
@@ -121,107 +141,138 @@ const UserProfileFeatureTwo = ({
   ];
 
   return (
-    <div>
-      {openSucessModal ? (
-        <PopupModal
-          onClickHandle={() => {
-            setIsSecondFeatureModalOpen(false);
-            setOpenSucessModal(true);
-          }}
-          openModal={openSucessModal}
-          setOpenModal={setOpenSucessModal}
-          onClose={() => setIsSecondFeatureModalOpen(false)}
-          image={
-            <Image
-              src="/assets/icons/success-2-icon.svg"
-              width={400}
-              height={400}
-              alt="img"
-              className=""
-            />
-          }
-          title="تم إضافة الهدف بنجاح"
-          desc="ستصلك يوميا أسعار الافتتاج والاغلاق"
-        />
-      ) : (
-        ""
-      )}
-      {isSecondFeatureModalOpen ? (
-        <ModalUI
-          onClickHandle={() => {
-            setOpenSucessModal(true);
-            handleSubmit();
-          }}
-          onClose={() => setIsSecondFeatureModalOpen(false)}
-          isOpen={isSecondFeatureModalOpen}
-          title="إضافة هدف جديد"
-          button="إضافة هدف جديد"
-          content={
-            <FeatureTwoGoalModal
-              handleChange={handleChange}
-              handleSubmit={handleSubmit}
-              formData={formData}
-              options={options}
-              buttonTabs={buttonTabs}
-              activeButton={activeButton}
-              handleMemoryChange={handleMemoryChange}
-              isOpen={isSecondFeatureModalOpen}
-            />
-          }
-        />
-      ) : (
-        ""
-      )}
-      <div className="space-y-6">
-        <div className="w-full bg-[#F5F7F9] py-4 px-4 rounded-3xl space-y-4 border border-gray-300">
-          <div className="space-y-4">
-            <ArrowList
-              leftIcon={
-                <IconSwitch
-                  handleSwitch={handlePricesSwitch}
-                  isChecked={isPricesChecked}
-                />
-              }
-              cardStyle="p-5"
-              title="الأسعار المستهدفة للأسهم"
-              desc="تصلك على الواتساب رسائل بسعر الافتتاح والاغلاق يوميا"
-              icon={
-                <Image
-                  src="/assets/icons/target-price-icon.svg"
-                  width={25}
-                  height={25}
-                  alt="img"
-                  className="ml-5"
-                />
-              }
-              isChecked={isPricesChecked}
-              addPanel={
-                <PricingAddPanel
-                  feature="second"
-                  image={
-                    <Image
-                      src="/assets/icons/target-secondary-icon.svg"
-                      width={42}
-                      height={42}
-                      alt="img"
-                      className=""
-                    />
-                  }
-                  setIsSecondFeatureModalOpen={setIsSecondFeatureModalOpen}
-                  tableTitles={tableTitles}
-                  feedText="لم تقم بإضافة هدف"
-                  title="قائمة الأسعار المستهدفة"
-                  setSelectedItems={setSelectedItems}
-                  handleFeedClick={handleFeedClick}
-                  selectedItems={tableData}
-                  tableData={tableData}
-                />
-              }
-            />
+    <>
+      <div>
+        {successAlert == true && (
+          <MessageAlert
+            setOpenModal={setSuccessAlert}
+            title="نجاح"
+            message={successMessage}
+            alertStyle="fixed top-5 right-2 text-primaryColor bg-teal-50 "
+            icon={
+              <CheckCircleIcon
+                className="h-5 w-5 text-primaryColor"
+                aria-hidden="true"
+              />
+            }
+          />
+        )}
+        {errorAlert == true && (
+          <MessageAlert
+            setOpenModal={setErrorAlert}
+            title="خطأ"
+            message={errorMessage}
+            alertStyle="fixed top-5 right-2 text-redColor bg-red-50 "
+            icon={
+              <XCircleIcon
+                className="h-5 w-5 text-redColor"
+                aria-hidden="true"
+              />
+            }
+          />
+        )}
+      </div>
+      <div>
+        {openSucessModal ? (
+          <PopupModal
+            onClickHandle={() => {
+              setIsSecondFeatureModalOpen(false);
+              setOpenSucessModal(true);
+            }}
+            openModal={openSucessModal}
+            setOpenModal={setOpenSucessModal}
+            onClose={() => setIsSecondFeatureModalOpen(false)}
+            image={
+              <Image
+                src="/assets/icons/success-2-icon.svg"
+                width={400}
+                height={400}
+                alt="img"
+                className=""
+              />
+            }
+            title="تم إضافة الهدف بنجاح"
+            desc="ستصلك يوميا أسعار الافتتاج والاغلاق"
+          />
+        ) : (
+          ""
+        )}
+        {isSecondFeatureModalOpen ? (
+          <ModalUI
+            onClickHandle={() => {
+              setOpenSucessModal(true);
+              handleSubmit();
+            }}
+            onClose={() => setIsSecondFeatureModalOpen(false)}
+            isOpen={isSecondFeatureModalOpen}
+            title="إضافة هدف جديد"
+            button="إضافة هدف جديد"
+            content={
+              <FeatureTwoGoalModal
+                handleChange={handleChange}
+                handleSubmit={handleSubmit}
+                formData={formData}
+                options={options}
+                buttonTabs={buttonTabs}
+                activeButton={activeButton}
+                handleMemoryChange={handleMemoryChange}
+                isOpen={isSecondFeatureModalOpen}
+              />
+            }
+          />
+        ) : (
+          ""
+        )}
+        <div className="space-y-6">
+          <div className="w-full bg-[#F5F7F9] py-4 px-4 rounded-3xl space-y-4 border border-gray-300">
+            <div className="space-y-4">
+              <ArrowList
+                leftIcon={
+                  <IconSwitch
+                    handleSwitch={handlePricesSwitch}
+                    isChecked={isPricesChecked}
+                  />
+                }
+                cardStyle="p-5"
+                title="الأسعار المستهدفة للأسهم"
+                desc="تصلك على الواتساب رسائل بسعر الافتتاح والاغلاق يوميا"
+                icon={
+                  <Image
+                    src="/assets/icons/target-price-icon.svg"
+                    width={25}
+                    height={25}
+                    alt="img"
+                    className="ml-5"
+                  />
+                }
+                isChecked={isPricesChecked}
+                addPanel={
+                  <PricingAddPanel
+                    feature="second"
+                    image={
+                      <Image
+                        src="/assets/icons/target-secondary-icon.svg"
+                        width={42}
+                        height={42}
+                        alt="img"
+                        className=""
+                      />
+                    }
+                    setIsSecondFeatureModalOpen={setIsSecondFeatureModalOpen}
+                    tableTitles={tableTitles}
+                    feedText="لم تقم بإضافة هدف"
+                    title="قائمة الأسعار المستهدفة"
+                    setSelectedItems={setSelectedItems}
+                    handleFeedClick={handleFeedClick}
+                    selectedItems={tableData}
+                  />
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
