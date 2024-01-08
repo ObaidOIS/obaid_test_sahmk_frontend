60fa158f4a2edb10b6f618efd712e42b36c73377
Muskan Javed <64351437+muskan-javed@users.noreply.github.com>
2024-01-08 22:51:07 +0500

    userprofile icon correct.

--- src/components/widgets/InputFieldUI/index.js
+++ src/components/widgets/InputFieldUI/index.js
@@ -5,7 +5,7 @@ const InputFieldUI = ({label, placeholder, name, value, id, paramOne, paramTwo, 
     <div>
     {label ? <label htmlFor="text" className="block text-sm font-medium leading-6 text-gray-900">{label}</label> : "" }
     <div className="mt-2">
-      <input inputMode={inputmode} value={value} onKeyUp={(e) => {onKeyUpEvent == true && onkeyup(e, paramOne, paramTwo)}} id={id} onChange={handleChange} maxLength={maxlength} type="text" name={name} className={`block appearance-none w-full px-4 rounded-md border-0 py-1.5 text-gray-900 outline-none shadow-sm ring-1  ring-gray-300 placeholder:text-gray-400 focus:ring-2 ${isValid ? 'focus:ring-primaryColor' : 'focus:ring-redColor'} sm:text-sm sm:leading-6 ${buttonStyle}`} required={required} placeholder={placeholder} />
+      <input inputMode={inputmode} value={value} onKeyUp={(e) => {onKeyUpEvent == true && onkeyup(e, paramOne, paramTwo)}} id={id} onChange={handleChange} maxLength={maxlength} type="text" name={name} className={`block appearance-none w-full px-4 rounded-md border-0 py-1.5 text-gray-900 outline-none shadow-sm ring-1  ring-gray-300 placeholder:text-gray-400 focus:ring-2 ${isValid == undefined ? "focus:ring-primaryColor" : (isValid == true ? 'focus:ring-primaryColor' : 'focus:ring-redColor') } sm:text-sm sm:leading-6 ${buttonStyle}`} required={required} placeholder={placeholder} />
     </div>
   </div>
   )
