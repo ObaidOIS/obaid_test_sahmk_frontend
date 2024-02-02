7c1fdb16d85be3629cb5f732bc110b2c4b67519e
Muskan Javed <64351437+muskan-javed@users.noreply.github.com>
2024-02-02 12:55:09 +0500

    checking otp input

--- src/components/sections/OtpModal/index.js
+++ src/components/sections/OtpModal/index.js
@@ -10,6 +10,7 @@ import {
   mergeKeysIntoThird,
 } from "@/components/common/utils";
 import { useRouter } from "next/navigation";
+import OtpInput from "react-otp-input";
 
 const OtpModal = ({
   isOpen,
@@ -29,7 +30,8 @@ const OtpModal = ({
   );
   const [timerActive, setTimerActive] = useState(true);
   // State for each OTP input
-  const [otp, setOtp] = useState(Array(4).fill(""));
+  // const [otp, setOtp] = useState(Array(4).fill(""));
+    const [otp, setOtp] = useState("");
   const [otpId, setOtpId] = useState("");
 
   const phoneNumber = getFullPhoneNumber(userData);
@@ -63,7 +65,7 @@ const OtpModal = ({
       const response = await apiCall("/auth/otp/", "POST", data, previousPage);
 
       setOtpId(response.result.id);
-      console.log(response, "hello")
+      console.log(response, "hello");
       // setErrorAlert(true);
       // setErrorMessage("phone no correct karo");
       // onClose();
@@ -87,55 +89,55 @@ const OtpModal = ({
     setTimerActive(true); // Start the timer
   };
 
-  const handleInputChange = (value, index) => {
-    // Update the corresponding OTP digit
-    let newOtp = [...otp];
-    newOtp[index] = value;
-    setOtp(newOtp);
+  // const handleInputChange = (value, index) => {
+  //   // Update the corresponding OTP digit
+  //   let newOtp = [...otp];
+  //   newOtp[index] = value;
+  //   setOtp(newOtp);
 
-    if (value === "") {
-      // If the first digit is removed, focus on the same input
-      document.getElementById(`code-${index - 1}`)?.focus();
-    }
-    console.log(otp, newOtp[index], value,  newOtp,   "hello id its me")
+  //   if (value === "") {
+  //     // If the first digit is removed, focus on the same input
+  //     document.getElementById(`code-${index - 1}`)?.focus();
+  //   }
+  //   console.log(otp, newOtp[index], value,  newOtp,   "hello id its me")
 
-    if (index == 3 && value !== "") {
-      // if (newOtp.length == 4 && value !== "") {
-      // const code = (otp + value).replace(/,/g, '');
-      const code = newOtp.join("");
-      // console.log(otp, "hello id its me")
-      handleSubmit(code);
-    }
-  };
+  //   if (index == 3 && value !== "") {
+  //     // if (newOtp.length == 4 && value !== "") {
+  //     // const code = (otp + value).replace(/,/g, '');
+  //     const code = newOtp.join("");
+  //     // console.log(otp, "hello id its me")
+  //     handleSubmit(code);
+  //   }
+  // };
 
   const resendText = ` ${timer.minutes()}:${timer.seconds()}`;
 
-  const focusNextInput = (e, prevId, nextId, index) => {
-    const value = e.target?.value ? e.target.value : e;
+  // const focusNextInput = (e, prevId, nextId, index) => {
+  //   const value = e.target?.value ? e.target.value : e;
 
-    // Ensure input is numeric
-    if (!value || isNaN(value)) {
-      return; // early return if not a number
-    }
+  //   // Ensure input is numeric
+  //   if (!value || isNaN(value)) {
+  //     return; // early return if not a number
+  //   }
 
-    // Update OTP
-    handleInputChange(value, index);
+  //   // Update OTP
+  //   handleInputChange(value, index);
 
-    const inputLength = value.length;
-    if (inputLength === 1) {
-      document.getElementById(nextId)?.focus();
-    } else if (inputLength === 0) {
-      document.getElementById(prevId)?.focus();
-      handleInputChange("", index - 1);
-    }
-  };
+  //   const inputLength = value.length;
+  //   if (inputLength === 1) {
+  //     document.getElementById(nextId)?.focus();
+  //   } else if (inputLength === 0) {
+  //     document.getElementById(prevId)?.focus();
+  //     handleInputChange("", index - 1);
+  //   }
+  // };
 
   const handleSubmit = async (value) => {
     // Combine OTP digits into a single variable
     // const enteredOTP = otp.join("");
     const enteredOTP = value == "undefined" ? otp.join("") : value;
 
-    console.log(enteredOTP, "hello id")
+    console.log(enteredOTP, "hello id");
     if (otpId && enteredOTP.length === 4) {
       // Ensure otpId is set and OTP is complete
       const otpPayload = {
@@ -173,7 +175,7 @@ const OtpModal = ({
           // Redirect based on the previousPage
           if (previousPage === "signup") {
             currentPlan == "الباقة المجانية" ||
-              (currentPlan.title ? currentPlan.title : selectedOption) ==
+            (currentPlan.title ? currentPlan.title : selectedOption) ==
               "الباقة المجانية"
               ? router.push("/userprofile")
               : router.push("/auth/order");
@@ -188,80 +190,95 @@ const OtpModal = ({
     }
   };
 
-  const handleKeyUp = (e, prevId, nextId, index) => {
-    // Handle backspace
-    if ((e.code === "Backspace" || e.keyCode === 46 || e.key === 'Backspace' ) && index > 0) {
-      // document.getElementById(prevId)?.setAttribute("id", `code-${index - 1}`);
-      document.getElementById(prevId)?.focus();
-      handleInputChange("", index);
-      return;
+  // const handleKeyUp = (e, prevId, nextId, index) => {
+  //   // Handle backspace
+  //   if ((e.code === "Backspace" || e.keyCode === 46 || e.key === 'Backspace' ) && index > 0) {
+  //     // document.getElementById(prevId)?.setAttribute("id", `code-${index - 1}`);
+  //     document.getElementById(prevId)?.focus();
+  //     handleInputChange("", index);
+  //     return;
+  //   }
+  // };
+
+  // const [copied, setCopied] = useState(false);
+
+  // const handleCopyClick = () => {
+  //   const textarea = document.createElement('textarea');
+  //   textarea.value = "hello";
+  //   document.body.appendChild(textarea);
+
+  //   textarea.select();
+  //   document.execCommand('copy');
+
+  //   document.body.removeChild(textarea);
+
+  //   setCopied(true);
+  // };
+
+  // useEffect(() => {
+  //   if (copied) {
+  //     // Display any additional UI/notification for the user
+  //     alert('Text copied to clipboard!');
+  //     setCopied(false); // Reset copied state
+  //   }
+  // }, [copied]);
+
+  // const handlePaste = (e) => {
+  //   e.preventDefault();
+  //   const pastedData = e.clipboardData.getData('text/plain').trim();
+  //   const otpInputs = Array.from({ length: 4 }, (_, index) => `code-${index}`);
+
+  //   let enteredOTP = Array(4).fill(""); // Initialize enteredOTP as an array of empty strings
+
+  //   otpInputs.forEach((inputId, index) => {
+  //     const inputElement = document.getElementById(inputId);
+  //     if (inputElement) {
+  //       const newValue = pastedData[index] || ''; // Use pasted digit or empty string
+  //       inputElement.value = newValue;
+  //       enteredOTP[index] = newValue; // Update the corresponding index in enteredOTP
+  //       focusNextInput(
+  //         newValue,
+  //         `code-${index - 3}`, // Previous ID
+  //         `code-${index + 3}`, // Next ID
+  //         index // Current index for OTP
+  //       );
+  //     }
+  //   });
+
+  useEffect(() => {
+    console.log(otp, "otp value");
+    if(otp.length == 4){
+      handleSubmit(otp)
     }
-  };
+  
+  }, [otp])
+
+  //   // Now enteredOTP is an array with four digits
+  //   console.log(enteredOTP, "hello id its me");
+  //   const enteredOTPArray = enteredOTP.join("");
+  //   // You can optionally call the handleSubmit function here passing the enteredOTP
+  //   handleSubmit(enteredOTPArray);
+  //   // Or update the state if needed
+  //   // setOtp(enteredOTP); // Update the state with the enteredOTP array
+  //   // focusNextInput(...); // Focus on the next input if needed
+  // };
 
 
-
-    const [copied, setCopied] = useState(false);
-  
-    const handleCopyClick = () => {
-      const textarea = document.createElement('textarea');
-      textarea.value = "hello";
-      document.body.appendChild(textarea);
-  
-      textarea.select();
-      document.execCommand('copy');
-  
-      document.body.removeChild(textarea);
-  
-      setCopied(true);
-    };
-  
     useEffect(() => {
-      if (copied) {
-        // Display any additional UI/notification for the user
-        alert('Text copied to clipboard!');
-        setCopied(false); // Reset copied state
-      }
-    }, [copied]);
-
-
-  const handlePaste = (e) => {
-    e.preventDefault();
-    const pastedData = e.clipboardData.getData('text/plain').trim();
-    const otpInputs = Array.from({ length: 4 }, (_, index) => `code-${index}`);
-  
-    let enteredOTP = Array(4).fill(""); // Initialize enteredOTP as an array of empty strings
-  
-    otpInputs.forEach((inputId, index) => {
-      const inputElement = document.getElementById(inputId);
-      if (inputElement) {
-        const newValue = pastedData[index] || ''; // Use pasted digit or empty string
-        inputElement.value = newValue;
-        enteredOTP[index] = newValue; // Update the corresponding index in enteredOTP
-        focusNextInput(
-          newValue,
-          `code-${index - 3}`, // Previous ID
-          `code-${index + 3}`, // Next ID
-          index // Current index for OTP
-        );
-      }
+    const otpInputElements = document.querySelectorAll('.otpInput');
+    
+    otpInputElements.forEach((inputElement) => {
+      // Change autocomplete attribute for each input with class 'otpInput'
+      inputElement.setAttribute('autocomplete', 'one-time-code');
     });
-  
-    // Now enteredOTP is an array with four digits
-    console.log(enteredOTP, "hello id its me");
-    const enteredOTPArray = enteredOTP.join("");
-    // You can optionally call the handleSubmit function here passing the enteredOTP
-    handleSubmit(enteredOTPArray);
-    // Or update the state if needed
-    // setOtp(enteredOTP); // Update the state with the enteredOTP array
-    // focusNextInput(...); // Focus on the next input if needed
-  };
-  
+  }, []);
 
   return (
     <div>
       {/* <button onClick={handleCopyClick}>Copy Text</button> */}
       <div className="flex justify-center">
-        <Image loading="eager"  
+        <Image
+          loading="eager"
           src="/assets/icons/mail-fast.svg"
           width={32}
           height={32}
@@ -273,8 +290,20 @@ const OtpModal = ({
       <p className="text-sm font-medium text-center mt-8">
         يرجى إدخال الرمز المرسل إلى جوالك
       </p>
-      <div className="flex gap-4 my-5" dir="ltr">
-        {Array.from({ length: 4 }, (_, index) => (
+      <div className="gap-4 my-5" dir="ltr">
+        <OtpInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={4}
+          inputStyle={"block appearance-none otpInput !w-full rounded-md border-0 py-1.5 text-gray-900 outline-none shadow-sm ring-1  ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-primaryColor sm:text-sm sm:leading-6"}
+          shouldAutoFocus={true}
+          defaultValue=""
+          containerStyle="!grid grid-cols-4 gap-4"
+          // renderSeparator={<span> &nbsp;</span>}
+          renderInput={(props) => <input {...props} />}
+          // renderInput={(props) => <input autocomplete="one-time-code" id="otpInput" {...props} />}
+        />
+        {/* {Array.from({ length: 4 }, (_, index) => (
           <InputFieldUI
             // handleBeforeInput={(e)=>{handlePaste(e)}}
             // handleOnInput={(e)=>{handlePaste(e)}}
@@ -302,17 +331,18 @@ const OtpModal = ({
               );
             }}
           />
-        ))}
+        ))} */}
       </div>
       <div>
         <p
           onClick={() => {
             timer.asSeconds() > 0 ? "" : handleResend();
           }}
-          className={` ${timer.asSeconds() > 0
+          className={` ${
+            timer.asSeconds() > 0
               ? ""
               : " hover:text-darkGreyColor underline cursor-pointer"
-            } text-sm font-medium text-right`}
+          } text-sm font-medium text-right`}
         >
           {timer.asSeconds() > 0
             ? ` إعادة ارسال الرمز بعد ${resendText} `
