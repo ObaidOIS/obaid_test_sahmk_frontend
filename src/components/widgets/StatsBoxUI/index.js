88002e554002f503db16e794800b44009df98ae5
Muskan Javed <64351437+muskan-javed@users.noreply.github.com>
2024-02-15 15:42:20 +0500

    vertical scroll remove from stats cards

--- src/components/widgets/StatsBoxUI/index.js
+++ src/components/widgets/StatsBoxUI/index.js
@@ -7,7 +7,7 @@ const StatsBoxUI = ({ stat }) => {
     <div>
   <dl className="grid grid-cols-1 gap-5">
       <div key={stat.name} className={`flex items-start overflow-hidden rounded-2xl px-4 py-5 sm:p-6 ${stat.name == "نسبة التغير اليومي" ? stat.value >= 0 ? "bg-brightGreenColor " : "bg-brightRedColor" : "border border-brightGreyColor bg-whiteColor"}`}>
-        <div className="flex-1 ml-2 overflow-x-auto">
+        <div className="flex-1 ml-2 overflow-x-auto overflow-y-hidden">
           <dd className={`text-xl font-medium tracking-tight ${stat.name == "نسبة التغير اليومي" ? stat.value >= 0 ? "text-lightGreenColor " : "text-lightRedColor" : "text-darkColor"}`}>
             {stat.value == "-" ? stat.value : isNaN(stat.value) ? stat.value : Number(stat.value).toLocaleString('en-US')}
           </dd>
